8e967ae714844d49a71150346258c2671dce807c
Stephanie Csanky <csan0001@algonquinlive.com>
2014-01-23 10:29:05 -0500

    test

--- js/video.js
+++ js/video.js
@@ -4,7 +4,7 @@ var $placeholder = $('#video-placeholder');
 dialogPolyfill.registerDialog($dialog.get(0));
 
 $('#btn-open').on('click', function () {
-	$placeholder.html('<iframe src="//player.vimeo.com/video/84867335?color=23ba35&amp;autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+	$placeholder.html('<iframe src="//player.vimeo.com/video/81186918?color=23ba35&amp;autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
 	/* 
 		.get() returns the native Javascript element for us to access.
 		When using jQuery we don't have access to the original HTML elements directly, so we are using jQuery to manipulate them.
