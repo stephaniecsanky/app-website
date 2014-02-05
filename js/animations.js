97530addc7e76e746830017a2f23e9e4c33cd0f4
Stephanie Csanky <csan0001@algonquinlive.com>
2014-02-05 17:12:32 -0500

    testing

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,32 @@
+/* My animation file */
+var $win = $(window);
+var $sun = $('.sun');
+var $dipper = $('.dipper');
+var $feature = $('.feat-list img') /* Unlike other above variables, this is 4 separate imgs, a collection of images */
+
+$win.on('scroll', function () {
+	// console.log($win.scrollTop() );
+	$sun.css('transform', 'rotate(' +  $win.scrollTop() / 6  + 'deg)');
+});
+
+$('.dipper-section').waypoint(function () {
+	$dipper.addClass('js-dipper-animate');
+}, {offset: '60%'});
+
+$('.ships-section').waypoint(function () {
+	$feature.eq(0).addClass('js-feature-delay-1');
+	$feature.eq(2).addClass('js-feature-delay-3');
+	$feature.eq(3).addClass('js-feature-delay-2');
+	$feature.eq(5).addClass('js-feature-delay-6');
+	$feature.eq(4).addClass('js-feature-delay-4');
+	$feature.eq(6).addClass('js-feature-delay-5');
+	$feature.addClass('js-feature-animate');
+}, {offset: '60%'});
+
+
+
+
+
 var $feature = $('.feat-list img') /* Unlike other above variables, this is 4 separate imgs, a collection of images */
 
 
