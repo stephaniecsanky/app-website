390c5fe71083430e43147bc71b2dc791b574aa50
Stephanie Csanky <csan0001@algonquinlive.com>
2014-02-05 23:11:40 -0500

    Added a note

--- js/animations.js
+++ js/animations.js
@@ -17,6 +17,17 @@ var $features = $('.feat-list img'); /* Unlike other above variables, this is se
 
 
 
+/* NOTE: When I type this in the console in Chrome my animation works 
+
+var $features = $(".feat-list img"); $features.eq(0).addClass('js-feature-delay-1');
+	$features.eq(2).addClass('js-feature-delay-5');
+	$features.eq(3).addClass('js-feature-delay-2');
+	$features.eq(5).addClass('js-feature-delay-6');
+	$features.eq(4).addClass('js-feature-delay-4');
+	$features.eq(6).addClass('js-feature-delay-3');
+	$features.addClass('js-feature-animate');
+	
+	*/
 
 
 
