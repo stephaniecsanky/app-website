d80947c126660ec0b58d0b3f35fbffcd5c019581
Stephanie Csanky <csan0001@algonquinlive.com>
2014-02-05 21:48:08 -0500

    Test

--- js/animations.js
+++ js/animations.js
@@ -4,17 +4,10 @@ var $sun = $('.sun');
 var $dipper = $('.dipper');
 var $ships = $('.ships img') /* Unlike other above variables, this is 4 separate imgs, a collection of images */
 
-$win.on('scroll', function () {
-	// console.log($win.scrollTop() );
-	$sun.css('transform', 'rotate(' +  $win.scrollTop() / 4  + 'deg)');
-});
 
-$('.dipper-section').waypoint(function () {
-	$dipper.addClass('js-dipper-animate');
-}, {offset: '60%'});
 
 $('.ships-section').waypoint(function () {
-	$ships.eq(0).addClass('js-ships-feature-1');
+	$ships.eq(0).addClass('js-ships-delay-1');
 	$ships.eq(2).addClass('js-ships-delay-3');
 	$ships.eq(3).addClass('js-ships-delay-2');
 	$ships.addClass('js-ships-animate');
