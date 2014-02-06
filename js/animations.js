293d27f4ad190321dc01909d0353c8b6eeeb3a63
Stephanie Csanky <csan0001@algonquinlive.com>
2014-02-05 21:52:20 -0500

    TEST

--- js/animations.js
+++ js/animations.js
@@ -2,18 +2,18 @@
 var $win = $(window);
 var $sun = $('.sun');
 var $dipper = $('.dipper');
-var $ships = $('.ships img') /* Unlike other above variables, this is 4 separate imgs, a collection of images */
+var $features = $('.feat-list') /* Unlike other above variables, this is 4 separate imgs, a collection of images */
 
 
 
-$('.ships-section').waypoint(function () {
-	$ships.eq(0).addClass('js-feature-delay-1');
-	$ships.eq(2).addClass('js-feature-delay-3');
-	$ships.eq(3).addClass('js-feature-delay-2');
-	$ships.eq(5).addClass('js-feature-delay-1');
-	$ships.eq(4).addClass('js-feature-delay-3');
-	$ships.eq(6).addClass('js-feature-delay-2');
-	$ships.addClass('js-feature-animate');
+$('.features-section').waypoint(function () {
+	$features.eq(0).addClass('js-feature-delay-1');
+	$features.eq(2).addClass('js-feature-delay-3');
+	$features.eq(3).addClass('js-feature-delay-2');
+	$features.eq(5).addClass('js-feature-delay-1');
+	$features.eq(4).addClass('js-feature-delay-3');
+	$features.eq(6).addClass('js-feature-delay-2');
+	$features.addClass('js-feature-animate');
 }, {offset: '60%'});
 
 
