e7b1b508424d612de3db823ae98d4cf4761325e0
Stephanie Csanky <csan0001@algonquinlive.com>
2014-02-05 22:35:34 -0500

    Tried removing delay classes

--- js/animations.js
+++ js/animations.js
@@ -6,12 +6,7 @@ var $features = $('.feat-list img'); /* Unlike other above variables, this is se
 
 
 $('.features-section').waypoint(function () {
-	$features.eq(0).addClass('js-feature-delay-1');
-	$features.eq(2).addClass('js-feature-delay-5');
-	$features.eq(3).addClass('js-feature-delay-2');
-	$features.eq(5).addClass('js-feature-delay-6');
-	$features.eq(4).addClass('js-feature-delay-4');
-	$features.eq(6).addClass('js-feature-delay-3');
+
 	$features.addClass('js-feature-animate');
 }, {offset: '60%'});
 
