8011cac95059615431a8c87e6192a76116638f11
Stephanie Csanky <csan0001@algonquinlive.com>
2014-02-05 20:46:46 -0500

    test

--- js/animations.js
+++ js/animations.js
@@ -2,11 +2,11 @@
 var $win = $(window);
 var $sun = $('.sun');
 var $dipper = $('.dipper');
-var $feature = $('.feat-list img') /* Unlike other above variables, this is 4 separate imgs, a collection of images */
+var $ships = $('.ships img') /* Unlike other above variables, this is 4 separate imgs, a collection of images */
 
 $win.on('scroll', function () {
 	// console.log($win.scrollTop() );
-	$sun.css('transform', 'rotate(' +  $win.scrollTop() / 6  + 'deg)');
+	$sun.css('transform', 'rotate(' +  $win.scrollTop() / 4  + 'deg)');
 });
 
 $('.dipper-section').waypoint(function () {
@@ -14,29 +14,12 @@ $('.dipper-section').waypoint(function () {
 }, {offset: '60%'});
 
 $('.ships-section').waypoint(function () {
-	$feature.eq(0).addClass('js-feature-delay-1');
-	$feature.eq(2).addClass('js-feature-delay-3');
-	$feature.eq(3).addClass('js-feature-delay-2');
-	$feature.eq(5).addClass('js-feature-delay-6');
-	$feature.eq(4).addClass('js-feature-delay-4');
-	$feature.eq(6).addClass('js-feature-delay-5');
-	$feature.addClass('js-feature-animate');
+	$ships.eq(0).addClass('js-ship-delay-1');
+	$ships.eq(2).addClass('js-ship-delay-3');
+	$ships.eq(3).addClass('js-ship-delay-2');
+	$ships.addClass('js-ships-animate');
 }, {offset: '60%'});
 
 
 
-
-
-var $feature = $('.feat-list img') /* Unlike other above variables, this is 4 separate imgs, a collection of images */
-
-
-$('.features').waypoint(function () {
-	$feature.eq(0).addClass('js-feature-delay-1');
-	$feature.eq(2).addClass('js-feature-delay-3');
-	$feature.eq(3).addClass('js-feature-delay-2');
-	$feature.eq(5).addClass('js-feature-delay-6');
-	$feature.eq(4).addClass('js-feature-delay-4');
-	$feature.eq(6).addClass('js-feature-delay-5');
-	$feature.addClass('js-feature-animate');
-}, {offset: '60%'});
 
