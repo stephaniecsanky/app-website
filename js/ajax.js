1ed3503ed44411749c060688f8d40097fe59bc67
Stephanie Csanky <csan0001@algonquinlive.com>
2014-02-07 19:44:36 -0500

    testing ajax & json

--- js/ajax.js
+++ js/ajax.js
@@ -8,21 +8,20 @@ $('.btn-load').on('click', function (){   // Button for loading an html page, no
 var rand = Math.round( Math.random() * 3);
 var file = 'testimonial-' + rand + '.html'; /* creates full name of the file */
 
-$('.rand-veg').load(file);
+$('.rand-test').load(file);
 
 	// Automaticaly load the dino.json file as soon as the webpage loads.
-var dinoData = $.ajax('dino.json', {
+var quoteData = $.ajax('testimonials.json', {
 	// This tells jQuery we are loading a JSON file and to parse it properly
 	dataType: 'json'
 });
 
 
 // Because AJAX takes time to download things off the internet, .done will be triggered after jQuery is downloaded
-dinoData.done(function (data) {
-	var dino = Math.round( Math.random() * data.length - 1);
+quoteData.done(function (data) {
+	var quote = Math.round( Math.random() * data.length - 1);
 
-	$('.dino-name').html(data[dino].name);
-	$('.dino-period').html(data[dino].period);
-	$('.dino-diet').html(data[dino].diet);
+	$('.user-name').html(data[testimonials].name);
+	$('.app-quote').html(data[testimonials].quote);
 });
 
