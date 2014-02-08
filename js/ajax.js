2b371747c627238d314bc21100c88d34e17b1910
Stephanie Csanky <csan0001@algonquinlive.com>
2014-02-07 21:01:46 -0500

    shortened ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,21 +1,13 @@
 var $placeholder = $('.placeholder');
+
 var rand = Math.round( Math.random() * 3);
 var file = 'testimonial-' + rand + '.html'; /* creates full name of the file */
 
 $('.rand-test').load(file);
 
-	// Automaticaly load the dino.json file as soon as the webpage loads.
-var quoteData = $.ajax('testimonials.json', {
-	// This tells jQuery we are loading a JSON file and to parse it properly
-	dataType: 'json'
-});
-
-
-// Because AJAX takes time to download things off the internet, .done will be triggered after jQuery is downloaded
-quoteData.done(function (data) {
+$.get('testimonials.json', function(data){
 	var quote = Math.round( Math.random() * data.length - 1);
 
 	$('.user-name').html(data[testimonials].name);
 	$('.app-quote').html(data[testimonials].quote);
 });
-
